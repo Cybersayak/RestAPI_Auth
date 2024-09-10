7e37f4a134fed5a7a8903d43a1d0fa830709d44d
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-10 08:29:24 +0000

    TS

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.send(
-    "Hello Backend Devs Building RestAPI and Learning Authentication using Typescript"
+    "Hello Backend Devs Building RestAPI and Learning Authentication using Typescript from Code With Antonio"
   );
 });
 
